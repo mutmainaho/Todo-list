89fb1a2c03c74f2534e4b3bad6e38c8eaceb7868
mutmainaho <66721821+mutmainaho@users.noreply.github.com>
2020-09-08 15:06:02 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const  search = document.querySelector('.search input')
+
+const addTemplate = todo => {
+    
+    const html = `
+       <li class="list-group-item d-flex justify-content-between align-items-center ">
+          <span>${todo}</span> 
+            <i class="far fa-trash-alt delete"></i>  
+      </li>
+    `;
+        list.innerHTML += html;
+
+};
+
+addForm.addEventListener( 'submit', e => {
+    e.preventDefault();
+    const todos = addForm.add.value.trim();
+
+
+   
+    if(todos.length){ 
+    addTemplate(todos);  
+    addForm.reset();
+    }
+    });
+    list. addEventListener( 'click', e => {
+        if(e.target.classList.contains('delete')){
+            e.target.parentElement.remove();
+        }
+
+    });
+    search.addEventListener( 'keyup', () =>{
+          const term = search.value.trim();
+          
+    });
+
+
+
+    
+    
+
+
+
