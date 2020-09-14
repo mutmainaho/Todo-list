f2a73f5bf0ecd944255a5922b5a5a5b6a962e50e
mutmainaho <66721821+mutmainaho@users.noreply.github.com>
2020-09-14 12:41:14 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todo');
+const  search = document.querySelector('.search input')
+
+const addTemplate = todo => {
+    
+    const html = `
+       <li class="list-group-item d-flex justify-content-between align-items-center ">
+          <span>${todo}</span> 
+            <i class="far fa-trash-alt delete"></i>  
+      </li>
+    `;
+        list.innerHTML += html;
+
+};
+
+addForm.addEventListener( 'submit', e => {
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+
+
+   
+    if(todo.length){ 
+    addTemplate(todo);  
+    addForm.reset();
+    }
+    });
+    list. addEventListener( 'click', e => {
+        if(e.target.classList.contains('delete')){
+            e.target.parentElement.remove();
+        }
+
+
+        const filterTodo= (term) => { 
+            Array.form(list.children)
+            .filter((todo) => !todo.textContent.include(term))
+            .forEach ((todo) => todo.classList.add('filtered'));
+
+            Array.form(list.children)
+            .filter((todo) => !todo.textContent.include(term))
+            .forEach ((todo) => todo.classList.add('filtered'));
+        }
+
+
+    });
+    search.addEventListener( 'keyup', () =>{
+          const term = search.value.trim();
+          filterTodo = (term);
+          
+    });
+
+
+
+    
+    
+
+
+
